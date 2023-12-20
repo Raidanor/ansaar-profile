cae69adc82b4dac08a35232fcb9a30f691afd218
Raidanor <silverstallion30@gmail.com>
2023-12-20 22:26:44 +0400

    filled in Layout with info

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -8,8 +8,19 @@ const Layout =() =>
             <h1>Welcome</h1>
             <div class="row">
                 <div class="col">
-                    I am Ansaar
-                    <p>This is a sample website I'm making with react</p>
+                    <p>
+                        I am Ansaar<br />
+                        This is a sample website I'm making with react
+                    </p>
+                    <br />
+                    <p>
+                        I am a Computer Science student and I am looking for a job in the tech industry
+                        <br />
+                        I've made a game using Unoty and you can view and download it using the "<u>Game</u>" link on the bottom right
+                        <br />
+                        <br />
+                        A copy of my resume can also be viewed on the bottom
+                    </p>
 
                 </div>
             </div>
