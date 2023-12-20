6c3a796d24fee23f9a97afccfc28e586f7442bfb
Raidanor <silverstallion30@gmail.com>
2023-12-20 04:59:55 +0400

    fixed ripple lines

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,12 +2,14 @@ import React from 'react';
 const Contact = () => {
   let styleConfig = {padding: '10px', color:'rgba(255, 255, 255)'};
     return(
-      <div class = "nodal" style={styleConfig}>
+      <div class = "nodal min-vh-100" style={styleConfig}>
         <center>
-            <h1 class="pad">Contact</h1>
+            <h1 class="pad">Contact</h1><br />
             <a class = "pad">Personal E-mail: ansaarkhadaroo@gmail.com</a><br /><br />
             <a class = "pad">School E-mail: 160356k@acadiau.ca</a><br /><br />
-            <a class = "pad">LinkedIn: <a href="https://www.linkedin.com/in/ansaar-khadaroo-a4459121b/">Profile</a></a><br />
+            <a class = "pad">LinkedIn: <a href="https://www.linkedin.com/in/ansaar-khadaroo-a4459121b/">Profile</a></a><br /><br />
+            <a class = "pad"><a href="https://github.com/Raidanor">Github</a></a><br />
+
         </center>
       </div>
     )
--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -4,12 +4,12 @@ const Layout =() =>
 {
     let styleConfig = {padding: '10px', color:'rgba(255, 255, 255)'};
     return(
-        <div class="nodal container" style={styleConfig}>
+        <div class="nodal container min-vh-100" style={styleConfig}>
             <div class="row">
                 <div class="col">
                     I am Ansaar
                     <p>This is a sample website I'm making with react</p>
-                    
+
                 </div>
             </div>
         </div>
