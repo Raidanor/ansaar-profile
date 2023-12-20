1905205d7b5c3dac5b362d464547918d3d9694f1
Raidanor <silverstallion30@gmail.com>
2023-12-20 04:48:43 +0400

    fiddling with ripples

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -4,12 +4,14 @@ const Layout =() =>
 {
     let styleConfig = {padding: '10px', color:'rgba(255, 255, 255)'};
     return(
-        <div class="nodal" style={styleConfig}>
-            <center>
-                <h1>The Big Picture</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt voluptates rerum eveniet sapiente repellat esse, doloremque quod recusandae deleniti nostrum assumenda vel beatae sed aut modi nesciunt porro quisquam voluptatem.</p>
-                <script src="js/scripts.js"></script>
-            </center>
+        <div class="nodal container" style={styleConfig}>
+            <div class="row">
+                <div class="col">
+                    I am Ansaar
+                    <p>This is a sample website I'm making with react</p>
+                    
+                </div>
+            </div>
         </div>
     
     )
